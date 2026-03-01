934be3543cdc0dc0f3090c07d2f5a2131e1341b0
Lelouch1117 <lelouch2k06@gmail.com>
2026-03-01 21:01:22 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,30 @@
-psycopg2-binary
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_connection():
+    return psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
+
+def init_db():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS games (
+        id SERIAL PRIMARY KEY,
+        description TEXT NOT NULL
+    );
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS countries (
+        tag VARCHAR(10) PRIMARY KEY,
+        user_id BIGINT
+    );
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
