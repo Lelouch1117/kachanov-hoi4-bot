38c2023fa196fa8acf5fb8d81db63afcf01f97a5
Lelouch1117 <lelouch2k06@gmail.com>
2026-03-01 22:00:30 +0500

    Update database.py

--- database.py
+++ database.py
@@ -2,10 +2,13 @@ import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 def get_connection():
-    return psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
+    return psycopg2.connect(
+        DATABASE_URL,
+        cursor_factory=RealDictCursor
+    )
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def init_db():
     conn = get_connection()
